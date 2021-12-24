1746aa6fbfc416bda712aea88ab902dc7faa9020
Nicolás <nicojoaquin1998@gmail.com>
2021-12-23 21:18:51 -0300

    'start'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from '../src/img/logo-circular-texto.png';
+import logo from "../src/img/logo-circular-texto.png";
 
 function App() {
   return (
     <div className="container-fluid">
       <header className="container-fluid d-flex flex-column align-items-center mt-5">
-      <h1 className="text-center">Bienvenido al template simple de React</h1>
-         <div className="w-25">
-        <img src={logo} className="w-100" alt="logo" />
-         </div>
+        <h1 className="text-center">Bienvenido al template simple de React</h1>
+        <div className="w-25">
+          <img src={logo} className="w-100" alt="log" />
+        </div>
         <p>
           Edita <code>src/App.js</code> y salva el cambio para recargar.
         </p>
